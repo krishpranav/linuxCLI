f4f75138c6ec749fbe80910d668401585b3cd3a8
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2023-11-03 16:30:08 +0530

    linuxCLI: utils

---
+++ linuxCLI/utils.swift
@@ -0,0 +1,20 @@
+//
+//  utils.swift
+//  linuxCLI
+//
+//  Created by Krisna Pranav on 03/11/23.
+//
+
+import Foundation
+
+func initMemoryStruct<S>() -> S {
+    let pointer = UnsafeMutablePointer<S>.allocate(capacity: 1)
+    let memory = pointer.pointee
+    pointer.deallocate()
+    return memory
+}
+
+func enableRawMode(fileHandle: FileHandle) {
+    var raw: termios = initMemoryStruct()
+    tcgetattr(fileHandle.fileDescriptor, &raw)
+}
