7f278ecf05574253187912a0ac1b62b689409a63
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2023-11-03 16:30:38 +0530

    linuxCLI: utils

--- linuxCLI/utils.swift
+++ linuxCLI/utils.swift
@@ -8,13 +8,16 @@
 import Foundation
 
 func initMemoryStruct<S>() -> S {
-    let pointer = UnsafeMutablePointer<S>.allocate(capacity: 1)
-    let memory = pointer.pointee
-    pointer.deallocate()
-    return memory
+  let pointer = UnsafeMutablePointer<S>.allocate(capacity: 1)
+  let memory = pointer.pointee
+  pointer.deallocate()
+  return memory
 }
 
 func enableRawMode(fileHandle: FileHandle) {
-    var raw: termios = initMemoryStruct()
-    tcgetattr(fileHandle.fileDescriptor, &raw)
+  var raw: termios = initMemoryStruct()
+  tcgetattr(fileHandle.fileDescriptor, &raw)
+
+  raw.c_lflag &= ~UInt(ECHO | ICANON)
+  tcsetattr(fileHandle.fileDescriptor, TCSAFLUSH, &raw)
 }
---
+++ linuxCLI/virtualcommands.swift
@@ -0,0 +1,8 @@
+//
+//  virtualcommands.swift
+//  linuxCLI
+//
+//  Created by Krisna Pranav on 03/11/23.
+//
+
+import Foundation
